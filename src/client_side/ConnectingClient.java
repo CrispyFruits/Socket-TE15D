e9c1219d659b3119629460a614b836d47f25e1be
JaoK <Joakim.ohl@gmail.com>
2018-03-20 12:31:17 +0100

    client side

---
+++ src/client_side/ConnectingClient.java
@@ -0,0 +1,72 @@
+package client_side;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ConnectingClient {
+
+	public static String HOST = "localhost";
+	public static int PORT = 12345;
+
+	private PrintStream to_server;
+	private BufferedReader from_server;
+	private Scanner sc;
+	private String name;
+	private Socket socket;
+
+	public ConnectingClient() {
+
+		sc = new Scanner(System.in);
+		System.out.print("Skriv in ditt namn: ");
+		name = sc.nextLine();
+
+		System.out.println("CONNECTING TO SERVER " + HOST + " ON PORT " + PORT);
+
+		try {
+
+			socket = new Socket(HOST, PORT);
+
+			to_server = new PrintStream(socket.getOutputStream());
+			from_server = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+			to_server.println(name);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		Runnable input_r = () -> {
+
+			while (true) {
+
+				System.out.println("HAS LINE");
+				String input = sc.nextLine();
+				to_server.println(input);
+
+			}
+
+		};
+
+		new Thread(input_r).start();
+
+		Runnable output_r = () -> {
+
+			while (true) {
+				try {
+					System.out.println(from_server.readLine());
+				} catch (IOException e) {
+					System.out.println("LOST CONNECTION TO SERVER");
+					System.exit(0);
+				}
+			}
+
+		};
+
+		new Thread(output_r).start();
+
+	}
+}
---
+++ src/client_side/client_connect.java
@@ -0,0 +1,11 @@
+package client_side;
+
+public class client_connect {
+
+	public static void main(String[] args) {
+
+		new ConnectingClient();
+
+	}
+
+}
